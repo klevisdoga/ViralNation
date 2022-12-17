b7fa52d2e0f279b737fc393f93a5cf99893dda7a
klevisdoga <klevisdoga@Mac-mini.local>
2022-12-17 15:39:21 -0500

    UPDATE: changed placement of 'add' button

--- src/components/PlanetList.tsx
+++ src/components/PlanetList.tsx
@@ -11,6 +11,7 @@ import {
 } from '@mui/material';
 import AddModal from './AddModal';
 import UpdateModal from './UpdateModal';
+import { Container } from '@mui/system';
 
 const GET_PLANETS = gql`
   query GetPlanets {
@@ -30,70 +31,75 @@ export default function PlanetList() {
     const [showUpdateModal, setUpdateModal] = useState(false);
     const [planets, setPlanets] = useState([]);
     const [planetId, setPlanetId] = useState('');
-      
+
     useEffect(() => {
-        if(data) setPlanets(data.allPlanets.planets)
+        if (data) setPlanets(data.allPlanets.planets)
     }, [data])
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
-    
+
     return (
-        <List sx={{
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            flexWrap: 'wrap'
-        }}>
-            <ListItem sx={{
-                width: 200,
-                justifyContent: 'center'
+        <>
+            <Container sx={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '2rem'
             }}>
                 <Button variant='contained' sx={{
                     backgroundColor: 'black',
-                    width: '100%'
+                    width: '50%',
+                    marginBottom: '2rem',
                 }}
-                onClick={() => setShowModal(true)}>
-        ADD
-      </Button>
-      {showModal && <AddModal 
-      setShowModal={setShowModal}
-      setPlanets={setPlanets}
-      planets={planets}
-      />}
-            </ListItem>
-            {planets.map((planet: any) => (
-                <ListItem key={planet.id} sx={{
-                    width: 200
-                }} 
-                className='planet-container'
-                onClick={() => {
-                    setUpdateModal(true)
-                    setPlanetId(planet.id)
-                }
-                }
-                >
-                    <ListItemText
-                        primary={planet.name}
-                        secondary={
-                            <>
-                                <Typography variant="body2" component="span">
-                                    Climates: {planet.climates}
-                                </Typography>
-                                <br />
-                                <Typography variant="body2" component="span">
-                                    Population: {planet.population || 'unknown'}
-                                </Typography>
-                            </>
+                    onClick={() => setShowModal(true)}>
+                    ADD
+                </Button>
+                {showModal && <AddModal
+                    setShowModal={setShowModal}
+                    setPlanets={setPlanets}
+                    planets={planets}
+                />}
+            </Container>
+            <List sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                flexWrap: 'wrap'
+            }}>
+                {planets.map((planet: any) => (
+                    <ListItem key={planet.id} sx={{
+                        width: 200
+                    }}
+                        className='planet-container'
+                        onClick={() => {
+                            setUpdateModal(true)
+                            setPlanetId(planet.id)
                         }
-                    />
-                </ListItem>
-            ))}
-            {showUpdateModal && <UpdateModal 
-      setUpdateModal={setUpdateModal}
-      setPlanets={setPlanets}
-      planets={planets}
-      planetId={planetId}
-      />}
-        </List>
+                        }
+                    >
+                        <ListItemText
+                            primary={planet.name}
+                            secondary={
+                                <>
+                                    <Typography variant="body2" component="span">
+                                        Climates: {planet.climates}
+                                    </Typography>
+                                    <br />
+                                    <Typography variant="body2" component="span">
+                                        Population: {planet.population || 'unknown'}
+                                    </Typography>
+                                </>
+                            }
+                        />
+                    </ListItem>
+                ))}
+                {showUpdateModal && <UpdateModal
+                    setUpdateModal={setUpdateModal}
+                    setPlanets={setPlanets}
+                    planets={planets}
+                    planetId={planetId}
+                />}
+            </List>
+        </>
     );
 }
